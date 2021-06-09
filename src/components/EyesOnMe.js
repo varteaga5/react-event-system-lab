29d51627817b758d5da58b1d477b1c1d37b72d35
vincent <vincent.arteaga@gmail.com>
2021-06-08 21:42:44 -0700

    assignment complete

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,26 @@
 // Code EyesOnMe Component Here
+import React, { Component } from 'react';
+
+
+class EyesOnMe extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {  }
+    }
+
+    handlesBlur = () => {
+        console.log('Hey! Eyes on me!')
+    }
+
+    handlesFocus = () => {
+        console.log('Good!' )
+    }
+
+    render() { 
+        return ( <div>
+            <button onFocus={this.handlesFocus} onBlur={this.handlesBlur} >BUTTON</button>
+        </div> );
+    }
+}
+ 
+export default EyesOnMe;
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,20 @@
 // Code Keypad Component Here
+import React, { Component } from 'react';
+
+
+class Keypad extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {  }
+    }
+
+    handleKeyUp = (e) => {
+        console.log('Entering password...')
+    }
+
+    render() { 
+        return ( <input type="password" onKeyUp={this.handleKeyUp} />);
+    }
+}
+ 
+export default Keypad;
